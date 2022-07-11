2a6c2fe12a3cf28861a591df97972c43817fd254
candace pinero <candcae.pinero@gmail.com>
2022-07-10 21:21:11 -0400

    config deployment

--- horsecollector/settings.py
+++ horsecollector/settings.py
@@ -125,5 +125,5 @@ LOGIN_REDIRECT_URL = '/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# import django_heroku
-# django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
